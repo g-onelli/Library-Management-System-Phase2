9782d155af63b6b46b388dc7040d9e8cd1b614b6
jramsey33 <108947297+jramsey33@users.noreply.github.com>
2022-07-18 17:23:25 -0400

    Initial Commit

---
+++ src/main/java/com/springboot/backend/LibraryManagementSystemPhase2Application.java
@@ -0,0 +1,13 @@
+package com.springboot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemPhase2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemPhase2Application.class, args);
+	}
+
+}
