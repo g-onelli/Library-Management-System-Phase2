3761adcecd0f7b3869ad80d719d8d59794314f73
jramsey33 <108947297+jramsey33@users.noreply.github.com>
2022-07-28 14:26:30 -0400

    Changes to All controller api calls as well as adding DTO objects for each entitiy

--- src/main/java/com/springboot/backend/repository/RequestRepository.java
+++ src/main/java/com/springboot/backend/repository/RequestRepository.java
@@ -1,8 +1,6 @@
 package com.springboot.backend.repository;
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.springboot.backend.model.Request;
 
