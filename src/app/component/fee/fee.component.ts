e11734d3f69026b32cf75a450b3abe483c87dbc8
Ian Kelly <ikelly@uwyo.edu>
2022-08-09 16:16:33 -0400

    fees

--- src/app/component/fee/fee.component.ts
+++ src/app/component/fee/fee.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FeeModel } from 'src/app/model/fee.model';
+import { FeeService } from 'src/app/service/fee.service';
 
 @Component({
   selector: 'app-fee',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeeComponent implements OnInit {
 
-  constructor() { }
+  fees: FeeModel[];
+
+  constructor(private feeService: FeeService) { }
 
   ngOnInit(): void {
+    this.feeService.getFees().subscribe({
+      next: (data) => {
+        this.fees = data;
+        this.feeService.fee$.next(this.fees);
+      },
+      error: (e) => {
+        console.log(e);
+      }
+    })
+
+    this.feeService.fee$.subscribe(data => {
+      this.fees = data;
+    })
+  }
+
+  markPaid(id: number) {
+    console.log(id);
+    this.feeService.updateFee(this.fees[id]).subscribe({
+      next: (data) => {
+        let pName = this.fees[id].patronName;
+        this.fees[id] = data;
+        this.fees[id].patronName = pName;
+        this.feeService.fee$.next(this.fees);
+      }
+    });
   }
 
 }
---
+++ src/app/model/fee.model.ts
@@ -0,0 +1,9 @@
+import {Patron} from 'src/app/model/patron.model'
+
+export class FeeModel {
+    id?: number;
+    datePaid: string;
+    feeType: string;
+    total: number;
+    patronName: string;
+}
