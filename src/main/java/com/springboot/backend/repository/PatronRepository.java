1a11a4decbf48c0e1dae1a93a88f67c57ed2e961
jramsey33 <108947297+jramsey33@users.noreply.github.com>
2022-08-10 14:07:28 -0400

    Adding back end changes to local branch

--- src/main/java/com/springboot/backend/repository/PatronRepository.java
+++ src/main/java/com/springboot/backend/repository/PatronRepository.java
@@ -1,7 +1,6 @@
 package com.springboot.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.springboot.backend.model.Patron;
 
--- src/main/java/com/springboot/backend/repository/RequestRepository.java
+++ src/main/java/com/springboot/backend/repository/RequestRepository.java
@@ -2,6 +2,7 @@ package com.springboot.backend.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.springboot.backend.model.Request;
 
